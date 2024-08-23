ea7e48c1ca659cdd2285a6c6547dccb12e7aa7d0
LucasAlt40 <lucas.alt40@gmail.com>
2024-08-22 22:13:04 -0300

    feat: creating the initial structure to get data

--- src/components/BottomLogin/BottomLogin.tsx
+++ src/components/BottomLogin/BottomLogin.tsx
@@ -4,6 +4,7 @@ import apiCommonInstance from "../../api/apiCommonInstance";
 
 import style from "./index.module.css";
 import React, { useEffect, useState } from "react";
+import { Button } from "@chakra-ui/react";
 
 interface BottomLoginProps {
   children?: React.ReactNode;
@@ -13,7 +14,18 @@ const BottomLogin: React.FC<BottomLoginProps> = ({ ...props }) => {
   const [title, setTitle] = useState("Quem é você?");
   const [step, setStep] = useState(0);
 
-  const steps = [<></>, <></>];
+  const steps = [
+    <>
+      <Button colorScheme="red" variant="outline" rightIcon={<Shield />}>
+        Proprietário
+      </Button>
+
+      <Button colorScheme="red" rightIcon={<User />}>
+        Usuário
+      </Button>
+    </>,
+    <></>,
+  ];
 
   const getData = async () => {
     const data = await apiCommonInstance.post("auth/owner", {
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home.tsx';
-import Login from './pages/login/Login.tsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Login from "./pages/login/Login.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const router = createBrowserRouter([
   {
@@ -20,9 +18,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </StrictMode>
+);
